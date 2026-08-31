2d0c0997be226ceb15e2a4c0a3d1d67a4ac324b7
Jishant Modi <modijishant@gmail.com>
2026-08-31 23:30:27 +0530

    Add files via upload

--- Loops/whileLoop.cpp
+++ Loops/whileLoop.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter n:"<<endl;
+    cin>>n;
 
+    int count=1;
+    while(count<=n)
+    {
+        cout<<count;
+        count++;
+    }
+}
